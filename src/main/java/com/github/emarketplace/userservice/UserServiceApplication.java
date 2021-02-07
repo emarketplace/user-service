d77bdc91dcffc6a23d314c7b95d99ef37312225c
Muhammed Shaheer <ckshaheer2012@gmail.com>
2021-02-07 11:53:47 +0530

    profile test

--- src/main/java/com/github/emarketplace/userservice/UserServiceApplication.java
+++ src/main/java/com/github/emarketplace/userservice/UserServiceApplication.java
@@ -1,7 +1,9 @@
 package com.github.emarketplace.userservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @author Muhammed Shaheer
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableConfigurationProperties
 public class UserServiceApplication {
 
     public static void main(String[] args) {
